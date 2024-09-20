067943bfa2fceb3acbfaaa38c96a4dbd71193f18
Zaf4 <zafermolbio@gmail.com>
2024-09-20 04:50:55 +0300

    correct remote link upon ownership transfer

--- framex/_dicts/_constants.py
+++ framex/_dicts/_constants.py
@@ -3,6 +3,6 @@ from pathlib import Path
 _LOCAL_DIR = Path(__file__).parent.parent / "data"
 if not _LOCAL_DIR.exists():
     _LOCAL_DIR.mkdir()
-_REMOTE_DIR_GITHUB = "https://github.com/Zaf4/datasets/raw/main/feather/"
+_REMOTE_DIR_GITHUB = "https://github.com/datavil/datasets/raw/main/feather/"
 _EXTENSION = ".feather"
-_INFO_FILE = "https://github.com/Zaf4/datasets/raw/main/datasets_info.csv"
+_INFO_FILE = "https://github.com/datavil/datasets/raw/main/datasets_info.csv"
