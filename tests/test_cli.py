8794583f8f7dc5a21eff584f510857ce2d1526a4
Zaf4 <zafermolbio@gmail.com>
2024-11-13 04:32:16 +0300

    test for cache dir

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,20 +3,36 @@ import shutil
 import subprocess
 from pathlib import Path
 
+def red(text):
+    return f"\033[31m{text}\033[0m"
+
+def green(text):
+    return f"\033[32m{text}\033[0m"
+
+def blue(text):
+    return f"\033[34m{text}\033[0m"
+
 
 def run_command(command):  # noqa: D103
-    result = subprocess.run(command, shell=True, check=True)
-    return result
+    try:
+        print(green(command))
+        result = subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(red(e))
+        return None
 
+    return result
 
 def test_cli():
     """Test the CLI components."""
     # test get
-    Path("data").mkdir()
     run_command("fx get titanic --dir ./data --format parquet")
 
     # test multiple get
     run_command("fx get titanic mpg --dir ./data --format csv")
+
+    # test get with cache
+    run_command("fx get iris --dir cache --format feather --overwrite")
 
     # test list
     run_command("fx list")
@@ -26,20 +42,24 @@ def test_cli():
 
     # test about multiple
     run_command("fx about titanic mpg")
+
+    # test an invalid command
+    run_command("fx invalid")
+
     return
 
 
 def main():  # noqa: D103
+    # Remove 'data' directory if it exists, then recreate it
     if Path("data").exists():
         shutil.rmtree("data", ignore_errors=True)
-        Path ("data").mkdir()
-    else:
-        Path ("data").mkdir()
+        print("Deleted data directory")
 
+    Path("data").mkdir(exist_ok=True)
     test_cli()
+    shutil.rmtree("data", ignore_errors=True)
 
     return
-
 
 if __name__ == "__main__":
     main()
