ad8c2676557ab1e430a7ec980877dfba52ff1c23
Zaf4 <zafermolbio@gmail.com>
2024-11-13 03:55:51 +0300

    new cache dolfer path

--- framex/_dicts/_constants.py
+++ framex/_dicts/_constants.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-_LOCAL_DIR = Path(__file__).parent.parent / "data"
+_LOCAL_DIR = Path.home() / ".cache" / "framex" / "datasets"
 if not _LOCAL_DIR.exists():
-    _LOCAL_DIR.mkdir()
+    _LOCAL_DIR.mkdir(parents=True)
 
+# feather: almost as effective compression as parquet, much faster to loading times
 _EXTENSION = "feather"
 _REMOTE_DIR_GITHUB = f"https://github.com/datavil/datasets/raw/main/{_EXTENSION}/"
 _INFO_FILE = "https://github.com/datavil/datasets/raw/main/datasets_info.csv"
