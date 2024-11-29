502729d3969f807fa0732917b2af619a92a190fa
Zaf4 <zafermolbio@gmail.com>
2024-11-30 02:57:27 +0300

    add caching for links

--- framex/_dicts/_all.py
+++ framex/_dicts/_all.py
@@ -1,8 +1,12 @@
-from framex._dicts._github import _GITHUB_DATASETS
+# from framex._dicts._github import _GITHUB_DATASETS
 from framex._dicts._local import _LOCAL_CACHES
 
 # more will be added later
 
+for i in range(200):
+    if i % 20 == 0:
+        print(i)
+    from framex._dicts._github import _GITHUB_DATASETS
 # merge dictionaries
 _REMOTE_DATASETS = {**_GITHUB_DATASETS}
 _DATASETS = {**_LOCAL_CACHES, **_REMOTE_DATASETS}
--- framex/_dicts/_github.py
+++ framex/_dicts/_github.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 import json
+import time
+from datetime import datetime
+from pathlib import Path
 
 import requests
 
-from framex._dicts._constants import _EXTENSION
+from framex._dicts._constants import _EXTENSION, _LOCAL_DIR
 
 # GitHub API URL for the directory contents
 _API_URL = f"https://api.github.com/repos/datavil/datasets/contents/{_EXTENSION}"
@@ -45,7 +48,7 @@ def _get_names(api_url: str) -> dict[str, str]:
 
     # Extract .feather files
     datasets = {
-        file["name"][:-len(f".{_EXTENSION}")]: file["download_url"]
+        file["name"][: -len(f".{_EXTENSION}")]: file["download_url"]
         for file in files
         if isinstance(file, dict) and file.get("name", "").endswith(f".{_EXTENSION}")
     }
@@ -53,4 +56,57 @@ def _get_names(api_url: str) -> dict[str, str]:
     return datasets
 
 
-_GITHUB_DATASETS = _get_names(_API_URL)
+def _save_json(datasets: dict[str, str]) -> None:
+    local_json = _LOCAL_DIR.parent / "datasets.json"
+    with Path.open(local_json, "w") as file_json:
+        json.dump(datasets, file_json, indent=4)
+
+    return
+
+
+def _read_json(local_json: Path) -> dict[str, str]:
+    local_json = _LOCAL_DIR.parent / "datasets.json"
+    with Path.open(local_json, "r") as file_json:
+        _datasets_dict = json.load(file_json)
+    return _datasets_dict
+
+
+def _cache_or_remote(url: str) -> dict[str, str]:
+    """
+    Decides whether to use the local cache or the remote datasets.
+
+    Above one hour uses remote API
+    Less than one hour uses local cache
+
+    Parameters
+    ----------
+    url : str
+        The URL of the GitHub API.
+
+    Returns
+    -------
+    dict
+        The names of the datasets mapped to their download URLs.
+
+    """
+    now = time.time()
+    modified = Path("datasets.json").stat().st_mtime
+    local_json = _LOCAL_DIR.parent / "datasets.json"
+
+    if local_json.exists():
+        if now - modified < 3600:
+            _datasets_dict = _read_json(local_json=local_json)
+            # print(f"Using cached datasets from {local_json}")
+        elif now - modified >= 3600:
+            _datasets_dict = _get_names(url)
+            _save_json(datasets=_datasets_dict)
+            # print(f"Updated cached datasets at {local_json}")
+    else:
+        _datasets_dict = _get_names(url)
+        _save_json(datasets=_datasets_dict)
+        # print(f"Created cached datasets at {local_json}")
+
+    return _datasets_dict
+
+
+_GITHUB_DATASETS = _cache_or_remote(url=_API_URL)
