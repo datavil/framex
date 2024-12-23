5dc8c02f7e6aff677108cfcd7640e85a5390fedd
Zaf4 <zafermolbio@gmail.com>
2024-12-23 21:38:11 +0300

    flex python req down to 3.8

--- framex/__init__.py
+++ framex/__init__.py
@@ -4,4 +4,4 @@ from framex.datasets import about, available, get_url, load
 
 __version__ = importlib.metadata.version("framex")
 
-__all__ = ["about", "available", "load", "get_url", "__version__"]
+__all__ = ["about", "available", "load", "get_url"]
