2f6ec14cf9e5496da2e610fe2b5be5c17b0e9b73
Zaf4 <zafermolbio@gmail.com>
2025-07-19 19:20:23 +0300

    re-format loading as import

--- framex/__init__.py
+++ framex/__init__.py
@@ -1,5 +1,4 @@
 import importlib.metadata
-from typing import TYPE_CHECKING
 
 from framex.datasets import about, available, get_url, load
 
@@ -7,12 +6,31 @@ __version__ = importlib.metadata.version("framex")
 
 __all__ = ["about", "available", "load", "get_url"]
 
-if TYPE_CHECKING:
-    from polars import DataFrame
+from polars import DataFrame
 
 
 def __getattr__(name: str) -> DataFrame:
+    """
+    Allow loading datasets importing as attributes.
+
+    e.g. `from framex import iris`.
+
+    This is equivalent to `framex.load("iris")`.
+
+    Parameters
+    ----------
+    name : str
+        The name of the dataset to load.
+
+    Returns
+    -------
+    DataFrame
+        The dataset as a Polars DataFrame.
+    """
     if name in available()["remote"]:
-        return load(name=name)
-    msg = f"`framex` has no attribute or dataset `{name}`"
-    raise AttributeError(msg)
+        dataset = load(name=name)
+    else:
+        msg = f"`framex` has no attribute or dataset `{name}`"
+        raise AttributeError(msg)
+
+    return dataset
